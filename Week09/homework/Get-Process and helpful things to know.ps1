aab5fe44826e7a1f4df5b525084e0cddfe3d5443
ConnorMerchant <59942710+ConnorMerchant@users.noreply.github.com>
2021-03-27 22:12:07 -0400

    Add files via upload
    Completed week 9

---
+++ Week09/homework/Get-Process and helpful things to know.ps1
@@ -0,0 +1,20 @@
+﻿#Description: Using Get-proccess and Get-service
+
+#Getting procccesses that are running then exporeting them to csv
+
+Get-Process | Select-Object ProcessName, Path, ID | `
+Export-Csv -Path "C:\Users\Daedalus\Desktop\myprocesses.csv" -NoTypeInformation
+
+# Getting services and exporting to csv
+
+Get-Service | Where { $_.Status -eq "Running" } | `
+Export-Csv -Path "C:\Users\Daedalus\Desktop\myservices.csv" -NoTypeInformation
+
+
+# Specifically targetting one process 
+#ps | where { $_.ProcessName -eq "Chrome" } | select ProcessName, ID, Path
+
+#use Get-Help to get help and it will take you down the path to help figure out what you want
+
+
+
