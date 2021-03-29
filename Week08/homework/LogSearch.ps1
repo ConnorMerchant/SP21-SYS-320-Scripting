c9c7062482e6ce0ef28306a3451342ec3c95925f
ConnorMerchant <59942710+ConnorMerchant@users.noreply.github.com>
2021-03-29 10:08:31 -0400

    Update LogSearch.ps1

--- Week08/homework/LogSearch.ps1
+++ Week08/homework/LogSearch.ps1
@@ -7,6 +7,10 @@
  #Create a prompt to allow a user to selec the log view
 
  $readLog = Read-Host -Prompt "Please select a log to review from the list above"
+ 
+ 
+ # Create a prompt that allows the user to specify a keyword or phrase to search on
+$readPhrase = Read-Host -Prompt "Please select a keyword or phrase to search on"
 
  #Print the resuslts of the logs
 
@@ -17,5 +21,3 @@
  #Find a string from your event logs to search on 
  #solution bellow and that is pumped in as a variable in the print results statement above
 
- # Create a prompt that allows the user to specify a keyword or phrase to search on
-$readPhrase = Read-Host -Prompt "Please select a keyword or phrase to search on"
